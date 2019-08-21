c90f9c9475d5d0e7286697625d65ced76e3d0b15
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-21 16:14:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+  
+  data.each do |col_gen_liv, hash|
+    hash.each do |type, array|
+      array.each do |name|
+        
+        if !final.has_key?(name)
+          final[name] = {}
+        end
+        
+        if !final[name].has_key?(col_gen_liv)
+          final[name][col_gen_liv] = []
+        end
+        
+        if !final[name][col_gen_liv].include?(type)
+          final[name][col_gen_liv] << type.to_s
+        
+        end
+        
+      end
+    end
+  end
+  final
 end
