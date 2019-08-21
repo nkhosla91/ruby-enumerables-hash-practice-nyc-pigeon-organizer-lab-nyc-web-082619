0206e5aec477adaff83e0472d4d7fcf00d11dc63
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-21 15:55:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,16 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final = {}
+  
   data.each do |col_gen_liv, hash|
     hash.each do |type, array|
       array.each do |name|
-        final[name] = col_gen_liv
-      binding.pry  
+        
+        if !final.has_key?(name)
+          final[name] = {}
+          
+          binding.pry
+        end
       end
     end
   end
