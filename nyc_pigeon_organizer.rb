1d61ac9919cd8905b9b81338cf23e848ea97dd1b
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-21 15:41:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+  
+  data.each do |col_gen_liv, hash|
+    hash.each do |type, array|
+      array.each do |name|
+        
+      end
+    end
+  end
+  final
 end
